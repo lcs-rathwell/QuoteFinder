75159c8399d2b120b6f76e0ec330ae130dded71c
Jack Rathwell <jrathwell@lcs.on.ca>
2025-03-03 14:41:45 -0500

    Initial Commit

---
+++ QuoteFinder/QuoteFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteFinderApp.swift
+//  QuoteFinder
+//
+//  Created by Jack William Rathwell on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct QuoteFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
